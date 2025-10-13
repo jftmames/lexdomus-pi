38f4b342f73d0c603a4f705b94b5d08b68f8a663
Jose Fernández Tamame <jftamames@gmail.com>
2025-10-13 04:45:07 +0200

    Update __init__.py

--- verdiktia/__init__.py
+++ verdiktia/__init__.py
@@ -1,1 +1,8 @@
+# Paquete para la capa de inquiry/deliberación
+try:
+    from .inquiry_engine import decompose_clause
+except Exception:
+    decompose_clause = None
+
+__all__ = ["decompose_clause"]
 # Package init for verdiktia
