d5e270670e996e888167ae8df86f006e52d4e167
Jose Fernández Tamame <jftamames@gmail.com>
2025-10-13 04:45:25 +0200

    Update __init__.py

--- metrics_eee/__init__.py
+++ metrics_eee/__init__.py
@@ -1,1 +1,13 @@
+# Paquete para el scoring y logging EEE
+try:
+    from .scorer import score_eee
+except Exception:
+    score_eee = None
+
+try:
+    from .logger import append_log
+except Exception:
+    append_log = None
+
+__all__ = ["score_eee", "append_log"]
 # Package init for metrics_eee
