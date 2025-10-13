8b59308a890564500f44105130894121db57e66e
Jose Fernández Tamame <jftamames@gmail.com>
2025-10-13 04:45:44 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,8 @@
+# Marca app/ como paquete y opcionalmente reexporta analyze_clause
+try:
+    from .pipeline import analyze_clause
+except Exception:
+    analyze_clause = None
+
+__all__ = ["analyze_clause"]
 # Package init for app
